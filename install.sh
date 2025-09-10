ec2ee7c8ae8dcddb57b678d554e200c7384707af
SFBB <anzhe990206@outlook.com>
2025-09-10 22:47:12 +0800

    fix(script): fix installation script's link use relative path instead of absolute path.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 mkdir -p ~/.local/bin
-ln -s ./target/release/watching_record ~/.local/bin/watching_record
+ln -s "$(pwd)/target/release/watching_record" ~/.local/bin/watching_record
